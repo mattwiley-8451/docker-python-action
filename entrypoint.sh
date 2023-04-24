a6b10b7f70727a6cf1e51802fd4a2afd3f4e8566
Matt Wiley <matt.wiley@8451.com>
2023-04-24 12:53:59 -0400

    Simplifying the input definition process.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 
-SOME_FILE_PATH="${1}"
-SAMPLE_STRING="${2}"
-
-python3 /action/main.py -f "${SOME_FILE_PATH}" -s "${SAMPLE_STRING}"
+python3 /action/main.py "${@}"
--- main.py
+++ main.py
@@ -3,25 +3,23 @@ import json
 import sys
 from argsy import Argsy
 
+# NOTE: Order of the 'args' is important here. This order must match the order 
+# of the args defined in the action.yml run.args list
 CLI_ARGS="""
 program:
     name: docker-shell-action
     args:
         some_file_path:
-            cmd_type: option
-            flags: '-f|--some-file-path'
+            cmd_type: position
             required: true
         sample_string:
-            cmd_type: option
-            flags: '-s|--sample-string'
+            cmd_type: position
             required: true
 """
 
 def main():
-
     argsy = Argsy(config_str=CLI_ARGS)
-    parsed_args = argsy.parse_args(args=sys.argv[1:], print_result=True)
-
-    print(json.dumps(parsed_args.get('args')))
+    parsed_args = argsy.parse_args(args=sys.argv[1:]).get('args')
+    print(json.dumps(parsed_args))
 
 main()
