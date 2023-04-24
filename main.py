5b40b3e91dd8f577f46595ae3731bfc101fb3465
Matt Wiley <matt.wiley@8451.com>
2023-04-24 12:43:42 -0400

    Generalized setup.

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 import json
-# import requests
 import sys
 from argsy import Argsy
 
@@ -18,28 +17,11 @@ program:
             required: true
 """
 
-def call_artifact_search_api():
-    pass
-
-def build_request_body(build_name:str, build_number:str, artifact_repo:str) -> str:
-    body_dict = dict(
-        buildName=build_name,
-        buildNumber=build_number,
-        repos=[
-            artifact_repo
-        ]
-    )
-    return json.dumps(body_dict)
-
-
 def main():
 
     argsy = Argsy(config_str=CLI_ARGS)
     parsed_args = argsy.parse_args(args=sys.argv[1:], print_result=True)
 
     print(json.dumps(parsed_args.get('args')))
-    print(build_request_body("my-build","1.0.123-java","java-dev-local"))
-
-
 
 main()
